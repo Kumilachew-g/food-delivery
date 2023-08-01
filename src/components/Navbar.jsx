2cc26f779bcce6036a0650dd236cec9d38217326
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-01 14:00:20 +0300

    Refactor styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
   return (
     <div>
-      <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <nav className='navbar navbar-expand-lg'>
         <a className='navbar-brand' href='#'>
           Navbar
         </a>
@@ -22,22 +22,12 @@ function Navbar() {
           <ul className='navbar-nav'>
             <li className='nav-item active'>
               <a className='nav-link' href='#'>
-                Home <span className='sr-only'>(current)</span>
+                Home
               </a>
             </li>
             <li className='nav-item'>
               <a className='nav-link' href='#'>
                 Features
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='#'>
-                Pricing
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link disabled' href='#'>
-                Disabled
               </a>
             </li>
           </ul>
