68724e090968373dc83b8acd65b1d76ef876bc5c
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-02 16:30:57 +0300

    Add styling for the food component

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 
 function Food({ food }) {
   return (
     <div>
       <h1>{food.name}</h1>
+      <img
+        src={food.image}
+        className='img-fluid'
+        style={{ height: '200px', width: '200px' }}
+      />
     </div>
   );
 }
