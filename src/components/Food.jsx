77e0b03a6c8a7ab918987f4df95b040041b89046
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-02 16:40:25 +0300

    Add variants and prices with styling

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -10,6 +10,14 @@ function Food({ food }) {
         className='img-fluid'
         style={{ height: '200px', width: '200px' }}
       />
+      <div className='flex-container'>
+        <div className='w-100'>
+          <p>Variants</p>
+        </div>
+        <div className='w-100'>
+          <p>Prices</p>
+        </div>
+      </div>
     </div>
   );
 }
