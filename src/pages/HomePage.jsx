3e06fbe039d994d4a03f25316f157ad124243cc1
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-02 16:00:49 +0300

    Test the static data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import food from '../data/foodData';
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>{food.length}</h1>
+    </div>
+  );
 }
 
 export default HomePage;
