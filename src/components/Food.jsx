d73ce35f067a6258239bb4c7c18d5f6afd4158a5
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-03 14:32:20 +0300

    Add price and variant functionalities

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
+import React, { useState } from 'react';
 
 function Food({ food }) {
+  const [variant, setVariant] = useState('small');
+  const [quantity, setQuantity] = useState(1);
   return (
-    <div>
+    <div
+      style={{ margin: '60px' }}
+      className='shadow-lg p-3 mb-5 bg-white rounded'
+    >
       <h1>{food.name}</h1>
       <img
         src={food.image}
@@ -11,9 +16,15 @@ function Food({ food }) {
         style={{ height: '200px', width: '200px' }}
       />
       <div className='flex-container'>
-        <div className='w-100'>
+        <div className='w-100 m-1'>
           <p>Variants</p>
-          <select>
+          <select
+            className='form-control'
+            value={variant}
+            onChange={(e) => {
+              setVariant(e.target.value);
+            }}
+          >
             {food.variants.map((variant) => {
               return (
                 <option value={variant} key={variant}>
@@ -23,9 +34,15 @@ function Food({ food }) {
             })}
           </select>
         </div>
-        <div className='w-100'>
+        <div className='w-100 m-1'>
           <p>Quantity</p>
-          <select>
+          <select
+            className='form-control'
+            value={quantity}
+            onChange={(e) => {
+              setQuantity(e.target.value);
+            }}
+          >
             {[...Array(10).keys()].map((x, i) => {
               return (
                 <option value={i + 1} key={i}>
@@ -34,6 +51,16 @@ function Food({ food }) {
               );
             })}
           </select>
+        </div>
+      </div>
+      <div className='flex-container'>
+        <div className='m-1 w-100'>
+          <h1 className='mt-1'>
+            Price: {food.prices[0][variant] * quantity} Birr/-
+          </h1>
+        </div>
+        <div className='m-1 w-100'>
+          <button className='btn'>Add To Cart</button>
         </div>
       </div>
     </div>
