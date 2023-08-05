6dfe2dc8bf7fda45578d86814ffb7f2a3509d2b5
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-05 22:23:50 +0300

    Add modal content from react-bootstrap modal documentation

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,20 +1,31 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
 
 function Food({ food }) {
   const [variant, setVariant] = useState('small');
   const [quantity, setQuantity] = useState(1);
+
+  // Modal state
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div
       style={{ margin: '60px' }}
       className='shadow-lg p-3 mb-5 bg-white rounded'
     >
-      <h1>{food.name}</h1>
-      <img
-        src={food.image}
-        className='img-fluid'
-        style={{ height: '200px', width: '200px' }}
-      />
+      <div onClick={handleShow}>
+        {' '}
+        <h1>{food.name}</h1>
+        <img
+          src={food.image}
+          className='img-fluid'
+          style={{ height: '200px', width: '200px' }}
+        />
+      </div>
       <div className='flex-container'>
         <div className='w-100 m-1'>
           <p>Variants</p>
@@ -63,6 +74,24 @@ function Food({ food }) {
           <button className='btn'>Add To Cart</button>
         </div>
       </div>
+
+      {/*  modal */}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Modal body text goes here.</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className='btn' onClick={handleClose}>
+            CLOSE
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
