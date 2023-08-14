80dd1ebb0d0cebdd8e2dd8397f7a6a7191437528
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-14 21:57:06 +0300

    Enhance

--- client/src/redux/actions/foodActions.js
+++ client/src/redux/actions/foodActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const getAllFood = () => (dispatch) => {
   dispatch({ type: 'GET_FOOD_REQUEST' });
   try {
-    const response = axios.get('/api/food/getFood');
+    const response = axios.get('/api/food/get-food');
     console.log(response);
     dispatch({ type: 'GET_FOOD_SUCCESS' });
   } catch (error) {
