cbe5c7dbd305c0a22daaf67f9c70109e9a3c1789
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-15 10:47:05 +0300

    Create store files

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,3 +5,21 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { getAllFoodReducer } from './reducers/foodReducers';
+
+const finalReducer = combineReducers({
+  getAllFoodReducer: getAllFoodReducer,
+});
+
+const initialState = {};
+
+const composeEnhancers = composeWithDevTools({});
+
+const store = createStore(
+  finalReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
