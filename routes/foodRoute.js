0e78d50becbe11046ad1b6091bfcd4e902453370
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-15 12:04:08 +0300

    Add route functionalities

---
+++ routes/foodRoute.js
@@ -0,0 +1,15 @@
+const express = requre('express');
+const router = express.Router();
+
+const food = require('../models/foodModel');
+
+router.get('/', async (req, res) => {
+  try {
+    const food = await food.find();
+    res.send(food);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
