7d2fb9864cc9b9ea1083a896141bada5b5a69f1f
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-15 10:36:23 +0300

    Import necessary components

---
+++ client/src/redux/store.js
@@ -0,0 +1,7 @@
+import combineReducers from 'redux';
+
+import { createStore, applyMiddleware } from 'redux';
+
+import thunk from 'redux-thunk';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
