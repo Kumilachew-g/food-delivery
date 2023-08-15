f70ee35f89c04082dc9c59d7e8daa5a5b01818d9
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-15 13:19:14 +0300

    Add payload logic

--- client/src/redux/reducers/foodReducers.js
+++ client/src/redux/reducers/foodReducers.js
@@ -1,15 +1,18 @@
-export const getAllFoodReducer = (state = {}, action) => {
+export const getAllFoodReducer = (state = { foods: [] }, action) => {
   switch (action.type) {
     case 'GET_FOOD_REQUEST':
       return {
+        payload: true,
         ...state,
       };
     case 'GET_FOOD_SUCCESS':
       return {
+        payload: false,
         food: action.payload,
       };
     case 'GET_FOOD_FAILED':
       return {
+        payload: false,
         error: action.payload,
       };
     default:
