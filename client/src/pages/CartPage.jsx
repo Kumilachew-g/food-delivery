45c1f9ab89ed4588dc135c596f38c733264e3c2b
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-29 16:31:54 +0300

    Implement map functionalities

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import { useDispatch, useSelector } from 'react-redux';
 function CartPage() {
+  const cartState = useSelector((state) => state.cartReducer);
+  const cartItems = cartState.cartItems;
   return (
     <div>
-      <h1>Cart Page</h1>
+      <div className='row justify-content-center'>
+        <div className='col-md-6'>
+          <h2 style={{ fontSize: '30px' }}>My Cart</h2>
+
+          {cartItems.map((item) => {
+            return (
+              <div className='flex-container'>
+                <div>
+                  <h1>{item.name}</h1>
+                </div>
+                <div></div>
+                <div></div>
+              </div>
+            );
+          })}
+        </div>
+        <div className='col-md-4'></div>
+      </div>
     </div>
   );
 }
