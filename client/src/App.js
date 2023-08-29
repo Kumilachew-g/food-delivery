6843d474bb8a84867538c3423b4de1d8691e3725
Kumilachew-g <kumilachew1212@gmail.com>
2023-08-29 16:05:17 +0300

    Add route link to tha cart page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
+import CartPage from './pages/CartPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' exact element={<HomePage />} />
+          <Route path='/cart' element={<CartPage />} />
         </Routes>
       </Router>
     </div>
