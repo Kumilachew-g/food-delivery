7f4e224ddf714ff0a63f6d5da9cfaa1f696bb238
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-02 12:07:47 +0300

    Add register page form

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -3,7 +3,38 @@ import React from 'react';
 function Register() {
   return (
     <div>
-      <h1>Register Page</h1>
+      <div className='row justify-content-center mt-5'>
+        <div className='col-md-5 mt-5 text-start'>
+          <h2 className='text-center m-2' style={{ fontSize: '35px' }}>
+            Register
+          </h2>
+          <div>
+            <input
+              required
+              type='text'
+              placeholder='name'
+              className='form-control'
+            />
+            <input
+              required
+              type='text'
+              placeholder='email'
+              className='form-control'
+            />
+            <input
+              type='text'
+              placeholder='password'
+              className='form-control'
+            />
+            <input
+              type='text'
+              placeholder='confirm password'
+              className='form-control'
+            />
+            <button className='btn mt-3'>REGISTER</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
