67d1b14b5cf5584629a646664327ae61c9e05720
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-02 10:58:40 +0300

    Add routes for the register and login pages.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import CartPage from './pages/CartPage';
+import Register from './pages/auth/Register';
+import Login from './pages/auth/Login';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path='/' exact element={<HomePage />} />
           <Route path='/cart' element={<CartPage />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </Router>
     </div>
