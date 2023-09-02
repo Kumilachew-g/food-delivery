553bc97ebec78e898cda6082dd4625258ba1d24c
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-02 15:41:51 +0300

    Fix error and finalize user backend

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
-export const registerUser = (user) => (dispatch) => {
+export const registerUser = (user) => async (dispatch) => {
   dispatch({ type: 'USER_REGISTER_REQUEST' });
 
   try {
-    axios.post('/api/users/register', { user });
+    const response = await axios.post('/api/users/register', { user });
+    console.log(response);
     dispatch({ type: 'USER_REGISTER_SUCCESS' });
   } catch (error) {
-    dispatch({ type: 'USER_REGISTER_FAIL', payload: error });
+    dispatch({ type: 'USER_REGISTER_FAILED', payload: error });
   }
 };
