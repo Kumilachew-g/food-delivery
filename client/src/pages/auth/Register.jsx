736268cce3f650b3eef09b0369f4371492c2563e
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-04 20:21:32 +0300

    Add loading, success and error alert message in register page

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { registerUser } from '../../redux/actions/userActions';
+import Loading from '../../components/Loading';
+import Success from '../../components/Success';
+import Error from '../../components/Error';
 
 function Register() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+
+  const registerState = useSelector((state) => state.registerUserReducer);
+  const { loading, success, error } = registerState;
 
   const dispatch = useDispatch();
   function register() {
@@ -27,6 +33,10 @@ function Register() {
     <div>
       <div className='row justify-content-center mt-5'>
         <div className='col-md-5 mt-5 text-start shadow-lg p-3 mb-5 bg-white rounded'>
+          {loading && <Loading />}
+          {success && <Success success='User Registered Successfully ' />}
+          {error && <Error error='Email Already Registered' />}
+
           <h2 className='text-center m-2' style={{ fontSize: '35px' }}>
             Register
           </h2>
