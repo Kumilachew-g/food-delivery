b2ec6a0c6a8b68322fd37f39f2608bc7f4b24c92
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-04 12:06:45 +0300

    Add route to login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -19,4 +19,27 @@ router.post('/register', (req, res) => {
   }
 });
 
+router.post('/login', async (req, res) => {
+  // Handle login
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.find({ email, password });
+
+    if (user.length > 0) {
+      const currentUser = {
+        name: user[0].name,
+        email: user[0].email,
+        isAdmin: user[0].isAdmin,
+        _id: user[0]._id,
+      };
+      res.send(currentUser);
+    } else {
+      return res.status(400).json({ message: 'User login failed' });
+    }
+  } catch (error) {
+    return res.status(400).json({ message: 'Something went wrong' });
+  }
+});
+
 module.exports = router;
