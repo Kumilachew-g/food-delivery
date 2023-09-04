13b35858900e16b9886558de4693ae794533baac
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-04 12:05:27 +0300

    Update login page and implement authentication

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../redux/actions/userActions';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  function login() {
+    const user = { email, password };
+    dispatch(loginUser(user));
+  }
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <div className='row justify-content-center mt-5'>
+        <div className='col-md-5 mt-5 text-start'>
+          <h2 className='text-center m-2' style={{ fontSize: '35px' }}>
+            Login
+          </h2>
+          <div>
+            <input
+              required
+              type='text'
+              placeholder='email'
+              className='form-control'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              required
+              type='text'
+              placeholder='password'
+              className='form-control'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={login} className='btn mt-3'>
+              LOGIN
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
