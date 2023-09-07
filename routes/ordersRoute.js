8a3ec4babba0adf94851f01c6264b8327dfc6ec2
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-07 13:45:14 +0300

    Add order route

---
+++ routes/ordersRoute.js
@@ -0,0 +1,11 @@
+// import express
+const express = require('express');
+
+// import express router
+const router = express.Router();
+
+// add orders controller
+router.post('/placeorder', (req, res) => {});
+
+// export router
+module.exports = router;
