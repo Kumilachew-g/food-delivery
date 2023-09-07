1905ee820660e878ba6c837ce781956c53e3c09b
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-07 12:41:58 +0300

    Implement stripe in the front end part

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
-function Checkout() {
-  return <div>Checkout</div>;
+function handleToken(token, addresses) {
+  console.log({ token, addresses });
+}
+
+function Checkout({ subtotal }) {
+  return (
+    <div>
+      <StripeCheckout
+        amount={subtotal * 100}
+        shippingAddress
+        token={handleToken}
+        stripeKey='pk_test_51NneHgKlJn4JJiGpID4zcqvjQ8gkoDNybYJzUsTN3urnlwkTChPhIgpUlqNK6FvBQKpQx4FPbrGgQ0dKJuwVKp6S00bBL1JEtM'
+        currency='USD'
+      >
+        <button className='btn'>Pay Now</button>
+      </StripeCheckout>
+    </div>
+  );
 }
 
 export default Checkout;
