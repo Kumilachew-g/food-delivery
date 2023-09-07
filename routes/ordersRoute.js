e04eab9e0ab60b67155790ab727628e03f508875
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-07 13:54:17 +0300

    Add stripe secret key

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -4,8 +4,15 @@ const express = require('express');
 // import express router
 const router = express.Router();
 
+// import stripe
+const stripe = require('stripe')(
+  'sk_test_51NneHgKlJn4JJiGpvVyA8mgU5tIsg8pEM8lcTQwASnfCCzg4P6QI87qzPMr9vk9bqZV03drBYR5mXUu4IHS2JCNs00zrOkY7oc'
+);
+
 // add orders controller
-router.post('/placeorder', (req, res) => {});
+router.post('/placeorder', (req, res) => {
+  const { token, subtotal, currentUser, cartItems } = req.body;
+});
 
 // export router
 module.exports = router;
