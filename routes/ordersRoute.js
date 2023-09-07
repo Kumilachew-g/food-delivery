5ae71baa7cf9b3d659ab88e1aed89ce147126f73
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-07 15:59:58 +0300

    Fix errors

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -7,16 +7,16 @@ const stripe = require('stripe')(
 );
 
 // add orders controller
-router.post('/placeorder', (req, res) => {
+router.post('/placeorder', async (req, res) => {
   const { token, subtotal, currentUser, cartItems } = req.body;
 
   try {
-    const customer = stripe.customers.create({
+    const customer = await stripe.customers.create({
       email: token.email,
       source: token.id,
     });
 
-    const payment = stripe.charges.create(
+    const payment = await stripe.charges.create(
       {
         amount: subtotal * 100,
         currency: 'usd',
@@ -25,7 +25,14 @@ router.post('/placeorder', (req, res) => {
       },
       { idempotencyKey: uuidv4() }
     );
-  } catch (error) {}
+    if (payment) {
+      res.send('Payment Success');
+    } else {
+      res.send('Payment Failed');
+    }
+  } catch (error) {
+    return res.status(400).json({ message: 'Something went wrong' });
+  }
 });
 
 module.exports = router;
