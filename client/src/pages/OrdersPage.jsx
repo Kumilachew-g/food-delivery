8ba13dce309fa651084c3d1375078c2385e61e6c
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-11 15:40:09 +0300

    Dispatch get user orders

--- client/src/pages/OrdersPage.jsx
+++ client/src/pages/OrdersPage.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserOrders } from '../redux/actions/orderActions';
 
 function OrdersPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUserOrders());
+  });
   return (
     <div>
       <h2 style={{ fontSize: '35px' }}>My Orders</h2>
