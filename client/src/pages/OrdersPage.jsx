9b94c04d091d90a3b6e727ae6ee4246499ed3ea4
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-11 14:49:36 +0300

    Create orders page , route and import it into Navbar

---
+++ client/src/pages/OrdersPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OrdersPage() {
+  return (
+    <div>
+      <h2 style={{ fontSize: '35px' }}>Orders Page</h2>
+    </div>
+  );
+}
+
+export default OrdersPage;
