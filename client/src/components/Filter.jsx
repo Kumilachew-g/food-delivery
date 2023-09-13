e17778eaf22df3a1be1a14e6a0303fd1cbf32656
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-13 13:32:51 +0300

    Create filter component

---
+++ client/src/components/Filter.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+function Filter() {
+  const dispatch = useDispatch();
+  return (
+    <div className='container'>
+      <div className='row justify-content-center shadow-lg p-3 mb-5 bg-white rounded'>
+        <div className='col-md-3'>
+          <input
+            type='text'
+            className='form-control w-100'
+            placeholder='search foods'
+          />
+        </div>
+        <div className='col-md-3'>
+          <select className='form-control w-100 mt-2'>
+            <option value='all'>All</option>
+            <option value='yetsom'>Yetsom</option>
+            <option value='yefsik'>Yefsik</option>
+          </select>
+        </div>
+        <div className='col-md-3'>
+          <button className='btn w-100 mt-2'>FILTER</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
