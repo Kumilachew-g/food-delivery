0f71bb7435b6d7107186c7a074668fb8a0ac815a
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-13 15:56:45 +0300

    Add filterFood action

--- client/src/redux/actions/foodActions.js
+++ client/src/redux/actions/foodActions.js
@@ -10,3 +10,20 @@ export const getAllFood = () => async (dispatch) => {
     dispatch({ type: 'GET_FOODS_FAILED', payload: error });
   }
 };
+
+//
+
+export const filterFood = (searchKey, categoryKey) => async (dispatch) => {
+  var filteredFoods;
+  dispatch({ type: 'GET_FOODS_REQUEST' });
+  try {
+    const response = await axios.get('/api/foods/getallfoods');
+    console.log(response);
+    filteredFoods = response.data.filter((food) =>
+      food.name.toLowerCase().includes(searchKey)
+    );
+    dispatch({ type: 'GET_FOODS_SUCCESS', payload: response.data });
+  } catch (error) {
+    dispatch({ type: 'GET_FOODS_FAILED', payload: error });
+  }
+};
