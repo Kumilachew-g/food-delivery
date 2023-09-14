ad8f936664e78d16017da9118cebe43ebcca39fa
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-14 14:24:07 +0300

    Add isAdmin validation logic using useEffect

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function AdminPage() {
+  const userState = useSelector((state) => state.loginUserReducer);
+  const { currentUser } = userState;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!currentUser.isAdmin) {
+      window.location.href = '/';
+    }
+  }, [currentUser.isAdmin]);
+
   return (
     <div>
       <h2 style={{ fontSize: '35px' }}>Admin panel</h2>
