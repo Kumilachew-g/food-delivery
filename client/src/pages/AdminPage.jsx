d4c553a4d3592bf280d5c3076689a9d5bc824c60
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-14 14:10:43 +0300

    Create admin page

---
+++ client/src/pages/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AdminPage() {
+  return (
+    <div>
+      <h2 style={{ fontSize: '35px' }}>Admin panel</h2>
+    </div>
+  );
+}
+
+export default AdminPage;
