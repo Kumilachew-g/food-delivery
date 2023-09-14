88d60944982700593bec433ee7938fd18deedc89
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-14 14:44:02 +0300

    Add new row and column to enhance the page

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -14,21 +14,25 @@ function AdminPage() {
 
   return (
     <div>
-      <h2 style={{ fontSize: '35px' }}>Admin panel</h2>
-      <ul className='admin-functions'>
-        <li>
-          <a href=''>Users List</a>
-        </li>
-        <li>
-          <a href=''>Food List</a>
-        </li>
-        <li>
-          <a href=''>Add new food</a>
-        </li>
-        <li>
-          <a href=''>Orders List</a>
-        </li>
-      </ul>
+      <div className='row justify-content-center'>
+        <div className='col-md-10'>
+          <h2 style={{ fontSize: '35px' }}>Admin panel</h2>
+          <ul className='admin-functions'>
+            <li>
+              <a href=''>Users List</a>
+            </li>
+            <li>
+              <a href=''>Food List</a>
+            </li>
+            <li>
+              <a href=''>Add new food</a>
+            </li>
+            <li>
+              <a href=''>Orders List</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
