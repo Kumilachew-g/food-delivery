8dead067c31a90ca62d814b764aa5676fe623643
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-20 16:22:49 +0300

    Add the backend logic

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -12,4 +12,25 @@ router.get('/getallfoods', async (req, res) => {
   }
 });
 
+// Add food routes
+
+router.post('/addfood', async (req, res) => {
+  const food = req.body.food;
+
+  try {
+    const newFood = new food({
+      name: food.name,
+      image: food.image,
+      varient: ['small', 'medium', 'large'],
+      description: food.description,
+      category: food.category,
+      price: [food.prices],
+    });
+    await newFood.save();
+    res.send('New food added successfully');
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
+
 module.exports = router;
