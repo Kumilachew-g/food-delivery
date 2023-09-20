1fef6bf8e307263150a2126cc356ee48963de460
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-20 17:38:43 +0300

    Add loading, error and success components

--- client/src/pages/AddFood.jsx
+++ client/src/pages/AddFood.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addFood } from '../redux/actions/foodActions';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import Success from '../components/Success';
 
 function AddFood() {
   const [name, setName] = useState('');
@@ -10,7 +13,11 @@ function AddFood() {
   const [image, setImage] = useState('');
   const [description, setDescription] = useState('');
   const [category, setCategory] = useState('');
+
   const dispatch = useDispatch();
+
+  const addFoodState = useSelector((state) => state.addFoodReducer);
+  const { loading, error, success } = addFoodState;
 
   function formHandler(e) {
     e.preventDefault();
@@ -34,6 +41,9 @@ function AddFood() {
     <div>
       <div className='text-start'>
         <h2>Add food</h2>
+        {loading && <Loading />}
+        {error && <Error error='Something went wrong' />}
+        {success && <Success success='Food added successfully' />}
         <form onSubmit={formHandler}>
           <input
             className='form-control'
