e39495ee971dc7e2da597c6af17519822941f54f
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-21 15:01:41 +0300

    Add edit food logic

--- client/src/pages/EditFood.jsx
+++ client/src/pages/EditFood.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function EditFood() {
+  const { foodid } = useParams();
   return (
     <div>
       <h2>Edit Food</h2>
+      <h2>Food Id = {foodid}</h2>
     </div>
   );
 }
