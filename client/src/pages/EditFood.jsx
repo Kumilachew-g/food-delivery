caade93458d7181ce585f91478960a6dced58756
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-21 17:32:22 +0300

    Use useEffect to dispatch food by id

--- client/src/pages/EditFood.jsx
+++ client/src/pages/EditFood.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getFoodById } from '../redux/actions/foodActions';
 
 function EditFood() {
   const { foodid } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getFoodById(foodid));
+  }, [foodid]);
+
   return (
     <div>
       <h2>Edit Food</h2>
