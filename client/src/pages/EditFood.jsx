42accd12f242228dbaa062c1f2725ab7870a186a
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-22 10:24:24 +0300

    Add edit food page ui

--- client/src/pages/EditFood.jsx
+++ client/src/pages/EditFood.jsx
@@ -1,20 +1,120 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { getFoodById } from '../redux/actions/foodActions';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import Success from '../components/Success';
 
 function EditFood() {
   const { foodid } = useParams();
   const dispatch = useDispatch();
 
+  const [name, setName] = useState('');
+  const [smallPrice, setSmallPrice] = useState();
+  const [mediumPrice, setMediumPrice] = useState();
+  const [largePrice, setLargePrice] = useState();
+  const [image, setImage] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('');
+
   useEffect(() => {
     dispatch(getFoodById(foodid));
   }, [foodid]);
+
+  function formHandler(e) {
+    e.preventDefault();
+
+    const food = {
+      name,
+      image,
+      description,
+      category,
+      prices: {
+        small: smallPrice,
+        medium: mediumPrice,
+        large: largePrice,
+      },
+    };
+
+    console.log(food);
+    // dispatch(addFood(food));
+  }
 
   return (
     <div>
       <h2>Edit Food</h2>
       <h2>Food Id = {foodid}</h2>
+      <div className='text-start'>
+        <form onSubmit={formHandler}>
+          <input
+            className='form-control'
+            type='text'
+            placeholder='name'
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            type='text'
+            placeholder='small varient price'
+            value={smallPrice}
+            onChange={(e) => {
+              setSmallPrice(e.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            type='text'
+            placeholder='medium varient price'
+            value={mediumPrice}
+            onChange={(e) => {
+              setMediumPrice(e.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            type='text'
+            placeholder='large varient price'
+            value={largePrice}
+            onChange={(e) => {
+              setLargePrice(e.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            type='text'
+            placeholder='category'
+            value={category}
+            onChange={(e) => {
+              setCategory(e.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            type='text'
+            placeholder='description'
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+          <input
+            className='form-control'
+            type='text'
+            placeholder='image url'
+            value={image}
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+          />
+          <button className='btn mt-3' type='submit'>
+            Add Food
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
