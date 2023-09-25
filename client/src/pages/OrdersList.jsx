4925729d2e8012ef7e09f3eac06936c468a61974
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-25 12:31:41 +0300

    Dispatch get all orders

--- client/src/pages/OrdersList.jsx
+++ client/src/pages/OrdersList.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import Filter from '../components/Filter';
+import { getAllOrders } from '../redux/actions/orderActions';
 
 function OrdersList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllOrders());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Orders List</h1>
