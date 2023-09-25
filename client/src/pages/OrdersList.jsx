184fbf70e1c665e53d3159bd9a52946c2a8a6dbc
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-25 14:43:48 +0300

    Remove unused filter imports

--- client/src/pages/OrdersList.jsx
+++ client/src/pages/OrdersList.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
-import Filter from '../components/Filter';
 import { getAllOrders } from '../redux/actions/orderActions';
 
 function OrdersList() {
