ecfd5337977f02597791b8478bbdbfbbe268fc00
Kumilachew-g <kumilachew1212@gmail.com>
2023-09-26 11:16:13 +0300

    Add and implement getAllUserReducer

--- client/src/redux/reducers/userReducer.js
+++ client/src/redux/reducers/userReducer.js
@@ -29,3 +29,26 @@ export const loginUserReducer = (state = {}, action) => {
       return state;
   }
 };
+
+// Get all users actions for admin
+export const getAllUsersReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case 'GET_USERS_REQUEST':
+      return {
+        loading: true,
+        ...state,
+      };
+    case 'GET_USERS_SUCCESS':
+      return {
+        loading: false,
+        users: action.payload,
+      };
+    case 'GET_USERS_FAILED':
+      return {
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
